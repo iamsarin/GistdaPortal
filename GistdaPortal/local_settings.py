cfe61ae6ae7992888ce0c5891503d477a9b9070b
GistdaDev <keerati@gistda.or.th>
2015-09-22 12:17:29 +0900

    replace local_settings.py with original one.
    replace with original geonode local_setting.py.sample

--- GistdaPortal/local_settings.py
+++ GistdaPortal/local_settings.py
@@ -2,49 +2,45 @@ import os
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-ALLOWED_HOST = ['*']
-
-REGISTRATION_OPEN = True
-
 SITEURL = "http://localhost:8000/"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'geonode',
-        'USER': 'geonode',
-        'PASSWORD': 'geonode',
-    },
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'geonode',
+         'USER': 'geonode',
+         'PASSWORD': 'geonode',
+     },
     # vector datastore for uploads
-    'datastore': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+    'datastore' : {
+        #'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': '', # Empty ENGINE name disables 
         'NAME': 'geonode',
-        'USER': 'geonode',
-        'PASSWORD': 'geonode',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'USER' : 'geonode',
+        'PASSWORD' : 'geonode',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
 # OGC (WMS/WFS/WCS) Server Settings
 OGC_SERVER = {
-    'default': {
-        'BACKEND': 'geonode.geoserver',
-        'LOCATION': 'http://localhost:8080/geoserver/',
-        'PUBLIC_LOCATION': 'http://localhost:8080/geoserver/',
-        'USER': 'admin',
-        'PASSWORD': 'gistda@123',
-        'MAPFISH_PRINT_ENABLED': True,
-        'PRINT_NG_ENABLED': True,
-        'GEONODE_SECURITY_ENABLED': True,
-        'GEOGIG_ENABLED': False,
-        'WMST_ENABLED': False,
+    'default' : {
+        'BACKEND' : 'geonode.geoserver',
+        'LOCATION' : 'http://localhost:8080/geoserver/',
+        'PUBLIC_LOCATION' : 'http://localhost:8080/geoserver/',
+        'USER' : 'admin',
+        'PASSWORD' : 'geoserver',
+        'MAPFISH_PRINT_ENABLED' : True,
+        'PRINT_NG_ENABLED' : True,
+        'GEONODE_SECURITY_ENABLED' : True,
+        'GEOGIG_ENABLED' : False,
+        'WMST_ENABLED' : False,
         'BACKEND_WRITE_ENABLED': True,
-        'WPS_ENABLED': False,
-        'LOG_FILE': '%s/geoserver/data/logs/geoserver.log' % os.path.abspath(
-            os.path.join(PROJECT_ROOT, os.pardir)),
+        'WPS_ENABLED' : False,
+        'LOG_FILE': '%s/geoserver/data/logs/geoserver.log' % os.path.abspath(os.path.join(PROJECT_ROOT, os.pardir)),
         # Set to name of database in DATABASES dictionary to enable
-        'DATASTORE': '',  # 'datastore',
+        'DATASTORE': '', #'datastore',
     }
 }
 
@@ -72,4 +68,4 @@ CATALOGUE = {
 }
 
 # Default preview library
-# LAYER_PREVIEW_LIBRARY = 'geoext'
+#LAYER_PREVIEW_LIBRARY = 'geoext'
